7195aa1d32ac6cdba082d9d5deb68194310ed01c
pavitapramestri <151427927+pavitapramestri@users.noreply.github.com>
2024-03-20 10:12:27 +0700

    Add files via upload

---
+++ resources/views/personalweb.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Cat's Personal Website</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/app.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand">≽^•⩊•^≼</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="img">
+        <h1 class="img">Hello, I'm Milo</h1>
+        <p>A Gray and lazy cat.</p>
+        <img src="../images/3.jpg">
+    </div>
+
+    <section id="about">
+        <div class="container">
+            <h2>About Me</h2>
+            <p>I'm Milo, the epitome of laziness wrapped in a cloud of fluffy fur. 
+                With a coat as soft as freshly spun cotton and a penchant for lounging in sunbeams, 
+                I embody the art of relaxation. My days are spent in a languid haze, drifting from 
+                one cozy spot to another, my current ambition being to find the perfect balance between 
+                comfort and repose. My laziness is not a flaw but a refined skill, mastered to perfection 
+                as I purr contentedly in my own little realm of tranquility.</p>
+        </div>
+    </section>
+</body>
+</html>
